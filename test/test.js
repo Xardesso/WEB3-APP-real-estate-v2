85084cf3f1405bdc4dc6d7122225ef00cdea4944
Xardesso <marcino53605@gmail.com>
2023-05-07 01:22:20 +0200

    first test

--- test/test.js
+++ test/test.js
@@ -1,70 +1,63 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
+let owner;
+let bidder2;
+let bidContract;
+let realestate;
+
 describe("BidContract", function () {
-  let bidContract;
-  let owner;
-  let bidder1;
-  let bidder2;
-
-  beforeEach(async () => {
-    [owner, bidder1, bidder2] = await ethers.getSigners();
-
+  this.timeout(60000);
+  beforeEach(async function () {
     const BidContract = await ethers.getContractFactory("BidContract");
+    const REALESTATE = await ethers.getContractFactory("REALESTATE");
+
     bidContract = await BidContract.deploy();
     await bidContract.deployed();
+    realestate = await REALESTATE.deploy();
+    await realestate.deployed();
   });
 
-  it("should allow a bidder to place a bid and update the highest bid", async () => {
-    // Bidder1 places a bid
-    const tx = await bidContract
-      .connect(bidder1)
-      .bid({ value: 1000, gasPrice: ethers.utils.parseUnits("10", "gwei") });
+  it("should allow a bidder to place a bid and update the highest bid", async function () {
+    const [bidder1] = await ethers.getSigners();
+    const tx = await bidContract.connect(bidder1).bid({ value: 100000 });
 
     // Wait for the transaction to be mined
     await tx.wait();
 
     // Verify that the highest bid has been updated
-    const highestBid = await bidContract.hishghestbidcheck();
-    assert.equal(highestBid, 1000, "Highest bid should be updated");
-
-    // Verify that the bidder's balance has been decreased by the bid amount
-    const finalBalance = await ethers.provider.getBalance(bidder1.address);
-    const initialBalance = await ethers.provider.getBalance(bidder1.address);
-    assert.equal(
-      finalBalance.toString(),
-      initialBalance.sub(1000).toString(),
-      "Bid amount should be deducted from bidder's balance"
-    );
-  });
-  it("should transfer funds to the previous highest bidder when a new bid is placed", async function () {
-    const initialBid = ethers.utils.parseEther("1");
-    const newBid = ethers.utils.parseEther("2");
-
-    await bidContract.connect(bidder1).bid({
-      value: initialBid,
-      gasPrice: ethers.utils.parseUnits("10", "gwei"),
-    });
-
-    const balanceBefore = await ethers.provider.getBalance(bidder1.address);
-
-    await bidContract.connect(bidder2).bid({ value: newBid });
-
-    const balanceAfter = await ethers.provider.getBalance(bidder1.address);
-    expect(balanceAfter).to.equal(balanceBefore.add(initialBid));
+    const highestBid = await bidContract.hisghestbidcheck();
+    expect(highestBid.toNumber()).to.equal(100000);
   });
 
-  it("should only allow the owner to end the auction and mint the NFT to the highest bidder", async function () {
-    const initialBid = ethers.utils.parseEther("1");
+  //("should transfer funds to the previous highest bidder when a new bid is placed", async function () {
+  //const initialBid = ethers.utils.parseEther("1");
+  //const newBid = ethers.utils.parseEther("2");
 
-    await bidContract.connect(bidder1).bid({ value: initialBid });
-    await bidContract.connect(owner).endAuction();
+  //await bidContract.connect(bidder1).bid({
+  //  value: initialBid,
+  //  gasPrice: ethers.utils.parseUnits("10", "gwei"),
+  //});
 
-    const tokenOwner = await bidContract.ownerOf(0);
-    expect(tokenOwner).to.equal(bidder1.address);
+  //const balanceBefore = await ethers.provider.getBalance(bidder1.address);
 
-    await expect(bidContract.connect(bidder2).endAuction()).to.be.revertedWith(
-      "Only owner can end auction"
-    );
-  });
+  //await bidContract.connect(bidder2).bid({ value: newBid });
+
+  //const balanceAfter = await ethers.provider.getBalance(bidder1.address);
+  //expect(balanceAfter).to.equal(balanceBefore.add(initialBid));
+  //;
+
+  //("should only allow the owner to end the auction and mint the NFT to the highest bidder", async function () {
+  //const initialBid = ethers.utils.parseEther("1");
+
+  //await bidContract.connect(bidder1).bid({ value: initialBid });
+  //await bidContract.connect(owner).endAuction();
+
+  //const tokenOwner = await bidContract.ownerOf(0);
+  //expect(tokenOwner).to.equal(bidder1.address);
+
+  //await expect(bidContract.connect(bidder2).endAuction()).to.be.revertedWith(
+  //  "Only owner can end auction"
+  //);
+  //});
 });
