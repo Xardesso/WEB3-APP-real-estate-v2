e14224d3ea11b61e32a1801e0f0a8a5657e238f4
Xardesso <marcino53605@gmail.com>
2023-05-09 23:26:29 +0200

    workin secind test

--- scripts/end.js
+++ scripts/end.js
@@ -1,5 +1,5 @@
 async function main() {
-  const conadd = "0x317fABa74E26877099f1486f777bc6a99d239598";
+  const conadd = "0x57c50eA3A17079Ed8C1069C7E487cCcc93A51d9F";
 
   const Token = await ethers.getContractFactory("BidContract");
   const final = await Token.attach(conadd);
